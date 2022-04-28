a65e52884d583ab87420235ae752e9861301210d
JackyW29 <75390967+JackyW19@users.noreply.github.com>
2022-04-28 14:12:52 +0000

    done

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,72 @@
 import processing.core.PApplet;
+import processing.core.PImage;
+
+/** 
+ * Description: ant running around in grass and a yellow circle moving in a circular motion
+ * @author Wang J.
+ * 
+ */
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+	PImage imgBackground;
+  PImage imgAnt;
+
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(700, 700);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    // declare variables
+    float fltX = 100;
+    float fltY = 575;
+    float fltXSpeed = 5;
+    float fltYSpeed = 4;
   
-  // define other methods down here.
-}
+    float fltAntX = 350;
+    float fltAntY = 45;
+    float fltAntXSpeed = 4;
+    float fltAntYSpeed = 4;
+
+    float fltCircle;
+    float fltRadius = 275;
+
+  public void setup() {
+    // load images from files 
+    imgBackground = loadImage("grass.jpeg");
+    imgBackground.resize(1000,1000);
+    imgAnt = loadImage("ant.png");
+    imgAnt.resize(50, 100);
+  }
+
+  public void draw() {
+
+    // declare background and relocate it
+    image(imgBackground, 0, -100);
+
+    // animate ant   
+    fltAntX += fltAntXSpeed;
+    fltAntY += fltAntYSpeed;
+
+    // set boundries 
+    if (fltAntX <= 0 || fltAntX > 625) {
+      fltAntXSpeed *= -1;
+    }
+
+    if (fltAntY <= 0 || fltAntY > 625) {
+      fltAntYSpeed *= -1;
+    }
+
+
+    // draw the circle and animate it
+    fill(165,42,42); 
+    ellipse(fltX, fltY, 50, 50);
+    
+    fltCircle += (Math.PI / 100);
+    fltX = (float) (300 + Math.sin(fltCircle) * fltRadius);
+    fltY = (float) (300 + Math.cos(fltCircle) * fltRadius);
+
+
+    image(imgAnt, fltAntX, fltAntY);
+    }
+  }
